d3881b34670f646b2d368b70d99604468b29b15b
rockyluke <rockyluke@offline.net>
2016-09-26 16:48:01 +0200

    Update copyright

--- source/conf.py
+++ source/conf.py
@@ -230,7 +230,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'masterpush.tex', 'masterpush Documentation', 'manual'),
+  (master_doc, 'masterpush.tex', 'MasterPush Documentation', [author], 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -259,8 +259,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'masterpush', 'masterpush Documentation',
-     [author], 1)
+    (master_doc, 'masterpush', 'MasterPush Documentation', [author], 1)
 ]
 
 # If true, show URL addresses after external links.
